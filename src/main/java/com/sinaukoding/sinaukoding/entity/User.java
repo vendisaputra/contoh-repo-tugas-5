368838ffb3006f97d7175fb5a956c6f515ace990
vendisaputra <vendisaputra@gmail.com>
2022-06-08 14:49:18 +0700

    tambahan materi mapper

--- src/main/java/com/sinaukoding/sinaukoding/entity/User.java
+++ src/main/java/com/sinaukoding/sinaukoding/entity/User.java
@@ -1,12 +1,10 @@
 package com.sinaukoding.sinaukoding.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.util.ArrayList;
 import java.util.Date;
 
 @Entity
